87deef6ead4730c190303ffe21da298a90aa972d
Zdzislaw <z.jaxa.m@gmail.com>
2024-05-12 11:57:50 +0200

    cleaned up, changed files to async access, added more quotes

--- bot.py
+++ bot.py
@@ -1,20 +1,19 @@
 import asyncio
-import datetime as datetime
+import datetime
 import json
 import math
-from datetime import datetime, timedelta
-
+import aiofiles
 import discord
-from discord import Member
-from discord.ext import commands
 
 import quote_to_image
 
-bot_token = 'MTIyNjA5NzEzNDY3NjM0ODk1OQ.GCxcgb.EHZgupFdoiqxKf-AZzIbO7nwvYZWrsdHWwKBOc'
-#bot_token = 'Nzc0NTczMDc0MTEyNzA4NjQ4.Gryj_9.sW0-C0WQ5AapulAEC0HM1HU__KlVNgdM9W41es'  # test one
-allowed_text_channels = ['tests', 'uwu']
-default_volume = 0.2
-default_length_time = 5
+from datetime import datetime, timedelta
+from discord import Member
+from discord.ext import commands, tasks
+
+bot_token = 'MTIyNjA5NzEzNDY3NjM0ODk1OQ.GCxcgb.EHZgupFdoiqxKf-AZzIbO7nwvYZWrsdHWwKBOc' # reik
+#bot_token = 'Nzc0NTczMDc0MTEyNzA4NjQ4.Gryj_9.sW0-C0WQ5AapulAEC0HM1HU__KlVNgdM9W41es'   # test one
+validating_user = 321297277773938690
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -33,14 +32,15 @@ async def on_ready():
         print(f'Synced {len(c)} commands')
     except Exception as e:
         print(f'got {e}')
+    # ticker.start()
 
     print(f'We have logged in as {bot.user.name}')
 
 
 @bot.tree.command(name='quote', description='Najlepsze kwestie jakie na przestrzeni lat padły na niniejszym serwerze')
 async def quote(interactions):
-    conf_file = open("jsons/config.json", mode='r+', encoding='UTF-8')
-    config = json.load(conf_file)
+    conf_file = await aiofiles.open("jsons/config.json", mode='r+', encoding='UTF-8')
+    config = json.loads(await conf_file.read())
     last_quote_time = tuple(map(int, config['last_quote_generated'].split(', ')))
     today = datetime.utcnow().timetuple()[0:3]
     if today <= last_quote_time:
@@ -50,14 +50,14 @@ async def quote(interactions):
         )
         return
 
-    quote_to_image.generate_image()
+    await quote_to_image.generate_image()
     await interactions.response.send_message(
         file=discord.File('text_image.png', 'Mądrość dnia.png')
     )
     config['last_quote_generated'] = str(today)[1:-1]
-    conf_file.seek(0)
-    json.dump(config, conf_file, indent=4, ensure_ascii=False)
-    conf_file.close()
+    await conf_file.seek(0)
+    await conf_file.writelines(json.dumps(config, indent=4, ensure_ascii=False))
+    await conf_file.close()
 
 
 @bot.tree.command(name='explain', description='Dodatkowe informacje \"lore\" ostatniej wypowiedzi')
@@ -74,6 +74,25 @@ async def explain(interactions):
     )
 
 
+@bot.tree.command(name='submit', description='Możliwość dodania własnego cytatu')
+async def submit(interactions, quote: str, author: str, date: str, additional_info: str = '-----'):
+    validating_member: Member = next((m for m in interactions.guild.members if m.id == validating_user), None)
+    if validating_member is None:
+        return
+    msg = {
+      'quote': quote,
+      'author': author,
+      'date': date,
+      'explanation': additional_info
+    }
+    res = str(msg).replace(',', ',\n').replace('{', '{\n').replace('}', '\n}').replace('\n', '\n\t').replace('\'', '\"')
+    res += f'\nsubmitted by {interactions.user.name}\n'
+    await validating_member.send(res)
+    await interactions.response.send_message(
+        "Request submitted", ephemeral=True, delete_after=60
+    )
+
+
 @bot.event
 async def on_member_update(before: Member, after: Member):
     role_id = 1236360986970161283  # 1225828166497730741
@@ -81,8 +100,8 @@ async def on_member_update(before: Member, after: Member):
 
     if before.get_role(role_id) is None:
         if after.get_role(role_id) is not None:
-            with open("jsons/punishments.json", mode='r+', encoding='UTF-8') as file:
-                data = json.load(file)
+            async with aiofiles.open("jsons/punishments.json", mode='r+', encoding='UTF-8') as file:
+                data = json.loads(await file.read())
                 if data['sentenced'].get(f'{after.id}') is None:
                     data['sentenced'].update({
                         f'{after.id}': {
@@ -90,18 +109,17 @@ async def on_member_update(before: Member, after: Member):
                             'time': f'{(datetime.utcnow() + timedelta(seconds=5)).timestamp()}'
                         }
                     })
-                    file.seek(0)
-                    json.dump(data, file, indent=4)
+                    await file.seek(0)
+                    await file.writelines(json.dumps(data, indent=4))
 
     elif before.get_role(role_id) is not None:
         if after.get_role(role_id) is None:
-            with open("jsons/punishments.json", mode='r+', encoding='UTF-8') as file:
-                data = json.load(file)
+            async with aiofiles.open("jsons/punishments.json", mode='w+', encoding='UTF-8') as file:
+                data = json.loads(await file.read())
                 if data['sentenced'].get(f'{after.id}') is not None and float(data['sentenced'].get(f'{after.id}')['time']) < datetime.utcnow().timestamp():
                     data['sentenced'].pop(f'{after.id}')
-                    file.seek(0)
-                    file.truncate()
-                    json.dump(data, file, indent=4)
+                    await file.seek(0)
+                    await file.writelines(json.dumps(data, indent=4, ensure_ascii=False))
                     await after.guild.get_channel(channel_id_to_shame).send(
                         f"```ansi\n"
                         f"Użytkownik [0;33m{before.name}[0m został oczyszczony z karnej roli [0;36m{before.get_role(role_id).name}\n"
@@ -119,105 +137,9 @@ async def on_member_update(before: Member, after: Member):
                     )
 
 
+@tasks.loop(seconds=2)
+async def ticker():
+    print("hello")
+
+
 bot.run(bot_token)
-"""
-async def start_count_to_dc(vc):
-    await sleep(10)
-    await lock_playing_state.acquire()
-    if is_playing:
-        lock_playing_state.release()
-        return
-    else:
-        await vc.disconnect()
-        lock_playing_state.release()
-
-
-@client.event
-async def on_ready():
-    with open('Quotes.txt', encoding='UTF-8') as f:
-        for line in f:
-            quotes_buffer.append(line)
-    print(f'We have logged in as {client.user}')
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.channel.name not in allowed_text_channels:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('' + check_volume('./Data/phoenix.mp3'))
-    elif message.content.startswith('/quote'):
-        await message.channel.send(quotes_buffer[random.Random.randint(random.Random(), 0, len(quotes_buffer) - 1)])
-
-
-@client.event
-async def on_voice_state_update(member, before, after):
-    global is_playing
-
-    await sleep(0.2)
-    # check if now members are 1 - just bot then dc
-    if before.channel is not None or after.channel is None or member.bot:
-        return
-    # if any(e.id == client.user.id for e in after.channel.members):
-    #    return
-    vc = None
-    # accessing volitile value "client.voice_clients"
-    try:
-        if len(client.voice_clients) > 0:
-            if client.voice_clients[0].channel != after.channel:
-                await client.voice_clients[0].disconnect()
-                vc = await after.channel.connect()
-        else:
-            vc = await after.channel.connect()
-    except discord.ClientException:
-        print("i got bobo with connect with multiple requests")
-        vc = await after.channel.connect()
-
-    # check if playing already
-    await lock_playing_state.acquire()
-    if is_playing:
-        lock_playing_state.release()
-        return
-    else:
-        is_playing = True
-        lock_playing_state.release()
-
-    # check if user in db to play
-    path_to_file = './Data/phoenix.mp3'
-    # standardize volume
-    check_volume(path_to_file)
-    # play the background sound
-    # change to load file as correct base with use of 'ffmpeg -i sample.avi -ss 00:03:05 -t 00:00:45.0 -q:a 0 -map a sample.mp3' from min 3.05 take 45 sec and convert to mp3
-    test = str(
-        subprocess.run(
-            './ffmpeg/bin/ffmpeg.exe -hide_banner -i \'.\Data\phoenix.mp3\' -filter:a volumedetect -f null /dev/null',
-            capture_output=True, stdin=subprocess.DEVNULL).stdout, "utf-8"
-    )
-    print(test)
-    vc.play(
-        discord.PCMVolumeTransformer(
-            discord.FFmpegPCMAudio(executable='ffmpeg/bin/ffmpeg.exe',
-                                   source=path_to_file),
-            # add check volume    '.\yt1s.com - there is no need to be upset.mp3'
-            default_volume
-        )
-    )
-    # stop after default length
-    await asyncio.sleep(default_length_time)
-    if vc.is_playing():
-        vc.stop()
-
-    # return lock to allow only one play
-    await lock_playing_state.acquire()
-    is_playing = False
-    lock_playing_state.release()
-    # start disconnect countdown
-    await start_count_to_dc(vc)
-    # await vc.disconnect()
-
-
-client.run(bot_token)"""
