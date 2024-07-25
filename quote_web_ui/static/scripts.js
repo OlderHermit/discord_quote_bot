9ea1782013f5c6b55e4a819d35bc6996db02868b
Zdzislaw <z.jaxa.m@gmail.com>
2024-07-25 11:46:10 +0200

    fixed background movement speed and dropped one collision detection rule

--- quote_web_ui/static/scripts.js
+++ quote_web_ui/static/scripts.js
@@ -10,6 +10,15 @@ const runes = [
     'ᛰ'
 ];
 
+const runes_sizes = {
+    'ᚠ':10.5, 'ᚡ':10.5, 'ᚢ':9.5, 'ᚣ':8.3, 'ᚤ':9.5, 'ᚥ':9.8, 'ᚦ':7.6, 'ᚧ':7.6, 'ᚨ':5.9, 'ᚩ':8.4, 'ᚪ':8.4, 'ᚫ':7.5, 'ᚬ':7.6, 'ᚭ':5.9, 'ᚮ':5.9, 'ᚯ':7.6,
+    'ᚰ':7.6, 'ᚱ':8.4, 'ᚲ':5.0, 'ᚳ':6.4, 'ᚴ':8.7, 'ᚵ':8.7, 'ᚶ':8.7, 'ᚷ':7.7, 'ᚸ':7.7, 'ᚹ':8.4, 'ᚺ':9.6, 'ᚻ':9.6, 'ᚼ':7.6, 'ᚽ':4.3, 'ᚾ':7.6, 'ᚿ':5.9,
+    'ᛀ':7.6, 'ᛁ':4.2, 'ᛂ':4.2, 'ᛃ':8.1, 'ᛄ':5.8, 'ᛅ':7.6, 'ᛆ':5.9, 'ᛇ':7.6, 'ᛈ':8.4, 'ᛉ':8.6, 'ᛊ':6.1, 'ᛋ':9.1, 'ᛌ':4.2, 'ᛍ':4.2, 'ᛎ':7.6, 'ᛏ':7.6,
+    'ᛐ':5.9, 'ᛑ':5.9, 'ᛒ':8.4, 'ᛓ':5.9, 'ᛔ':8.5, 'ᛕ':8.1, 'ᛖ':10.1, 'ᛗ':10.1, 'ᛘ':9.0, 'ᛙ':4.2, 'ᛚ':5.9, 'ᛛ':5.9, 'ᛜ':7.2, 'ᛝ':7.8, 'ᛞ':10.1, 'ᛟ':7.5,
+    'ᛠ':8.9, 'ᛡ':7.6, 'ᛢ':8.9, 'ᛣ':8.6, 'ᛤ':7.7, 'ᛥ':10.1, 'ᛦ':9.0, 'ᛧ':4.2, 'ᛨ':7.6, 'ᛩ':8.4, 'ᛪ':9.7, '᛫':4.7, '᛬':3.5, '᛭':10.3, 'ᛮ':7.6, 'ᛯ':9.0,
+    'ᛰ':9.1
+};
+
 readTextFile("static/authors.json", function (text) {
     data = JSON.parse(text);
     add_field();
@@ -172,8 +181,24 @@ function generate_rune_for_background(list) {
     let amount = Math.floor(Math.random() * 9) + 3;
     let x = Math.random() * 105 - 20;
     let y = Math.random() * 95;
+    let size = 0;
 
-    var retry_counter = 5;
+    let rune = document.createElement("div");
+
+    rune.className = "runes-container";
+
+    for(let i=0; i < amount; i++){
+        const character = document.createElement("div");
+        let symbol = runes[Math.floor(Math.random() * runes.length)];
+        size += runes_sizes[symbol];
+
+        character.className = "rune";
+        character.style = `animation-delay: ${delay+i*0.5}s`;
+        character.append(symbol);
+        rune.appendChild(character);
+    }
+
+    var retry_counter = 50;
     // console.log(`try have already saved ${list.length}`);
     // console.log(list.filter(e => Math.abs(e.top - y*vh/100) <= 16).length);
     // console.log(list.filter(e => Math.abs(e.top - y*vh/100) <= 16).filter(e => e.left < x*vw/100 && vw - e.right > x*vw/100).length);
@@ -181,36 +206,23 @@ function generate_rune_for_background(list) {
     while(list
         .filter(e => Math.abs(e.top - y*vh/100) <= 16)
         .filter(e => e.left < x*vw/100 && vw - e.right > x*vw/100)
-        .filter(e => e.left < x*vw/100 + 8.5*amount && vw - e.right > x*vw/100 + 8.5*amount)
+        // .filter(e => e.left < x*vw/100 + size && vw - e.right > x*vw/100 + size)
         .length > 0 && retry_counter > 0
     ){
-        // console.log('retry');
         x = Math.random() * 105 - 20;
         y = Math.random() * 95;
         retry_counter--;
     }
 
     if (retry_counter == 0){
-        // console.log('dupa');
         x = 0;
         y = 0;
-        amount = 0;
+        rune = document.createElement("div");
+        rune.style = `left: ${x}vw;top: ${y}vh;`;
+    } else {
+        rune.style = `left: ${x}vw;top: ${y}vh;animation: scrollAnimation ${size/4}s infinite linear;animation-delay: ${delay}s`;
     }
 
-
-    const rune = document.createElement("div");
-
-    rune.className = "runes-container";
-    rune.style = `left: ${x}vw;top: ${y}vh;animation: scrollAnimation ${amount*7}s infinite linear;animation-delay: ${delay}s`;
-
-    for(let i=0; i < amount; i++){
-        const character = document.createElement("div");
-
-        character.className = "rune";
-        character.style = `animation-delay: ${delay+i*0.5}s`;
-        character.append(runes[Math.floor(Math.random() * runes.length)]);
-        rune.appendChild(character);
-    }
     return rune;
 }
 
