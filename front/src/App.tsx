3dcb8cbe325ae8e1d7f2b59121052052c3de6a38
Zdzislaw <80225618+OlderHermit@users.noreply.github.com>
2025-07-09 16:07:33 +0200

    updated requirements.txt

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -20,7 +20,6 @@ function App() {
                 <Route path="/"
                     element={
                         <ProtectedRoute><Home /></ProtectedRoute>
-                        // <Home/>
                     }
                 />
 
