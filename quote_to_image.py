cdc5edc8617a10123461d31f84d29efb2b1bed72
Zdzislaw <80225618+OlderHermit@users.noreply.github.com>
2026-02-04 23:38:02 +0100

    removed seperate logic for quote generation

--- quote_to_image.py
+++ quote_to_image.py
@@ -1,11 +1,10 @@
 import math
 import random
-import re
 
 from PIL import Image, ImageDraw, ImageFont
 from PIL.ImageFont import FreeTypeFont
 from sqlalchemy import Engine, select
-from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.orm import Session
 
 from orm import Quote, Author, Sentence
 
@@ -34,34 +33,7 @@ def format_author_line(author, space_width, max_author_width, author_offset, che
     return author.id + ':' + ' ' * spaces_needed
 
 
-def split_to_size(quote: Quote, maxsize: int, check_font: ImageFont):
-    longest_size = 0
-    combined_size = 0
-    line = ''
-    result = []
-
-    space_width = check_size(' ', check_font)
-    text = quote.sentences[0].sentence
-    color = default_font_color
-
-    for c in text.split(' '):
-        if combined_size + check_size(c, check_font) > maxsize:
-            result.append((center(line, maxsize, check_font), color))
-            if longest_size < combined_size:
-                longest_size = combined_size
-            combined_size = check_size(c, check_font) + space_width
-            line = c + ' '
-        else:
-            combined_size += check_size(c, check_font) + space_width
-            line += c + ' '
-    if longest_size < combined_size:
-        longest_size = combined_size
-    result.append((center(line, maxsize, check_font), color))
-    result.append((center(generate_separator(longest_size, check_font), maxsize, check_font), color))
-    return result
-
-
-def split_to_size_dialogue(dialogue: Quote, maxsize: int, check_font: ImageFont):
+def split_to_size(dialogue: Quote, maxsize: int, check_font: ImageFont):
     author_offset = 5
     space_width, max_author_width, spaces_equal_max_author_width = calculate_basic_widths(dialogue, author_offset, check_font)
 
@@ -118,17 +90,8 @@ def generate_separator(longest_size: int, check_font: ImageFont):
     return '-'*seps
 
 
-def prepare_quote(quote: Quote, width: int, text_position: tuple[int, int], fonts: dict[str, FreeTypeFont]):
-    author: Author = quote.get_authors()[0]
-    centered = split_to_size(quote, width - text_position[0] * 2, fonts['base'])
-    centered.append(
-        (center(author.signature, width - text_position[0] * 2, fonts['base'], fonts['icon']), author.get_tuple_color())
-    )
-    return centered
-
-
 def prepare_dialogue(quote: Quote, width: int, text_position: tuple[int, int], fonts: dict[str, FreeTypeFont]):
-    centered = split_to_size_dialogue(quote, width - text_position[0] * 2, fonts['base'])
+    centered = split_to_size(quote, width - text_position[0] * 2, fonts['base'])
 
     for author in quote.get_authors():
         centered.append(
@@ -160,48 +123,47 @@ def reset_used_quotes(session: Session):
     session.commit()
     session.close()
 
+def generate_image_for_quote(quote: Quote):
+    width = 600
+    text_position = (50, 50)
+    text_color = (255, 255, 255)
+
+    fonts = {
+        "base": ImageFont.truetype("assets/Jaini-Regular.ttf", 36),
+        "icon": ImageFont.truetype("assets/SEGUIEMJ.ttf", 36),
+    }
+
+    centered = prepare_dialogue(quote, width, text_position, fonts)
+
+    image = Image.new("RGB", (width, 100 + 50 * len(centered)), (0x27, 0x29, 0x2E))
+    draw = ImageDraw.Draw(image)
+
+    for j, pair in enumerate(centered):
+        line, color = pair
+        x = text_position[0]
+        y = text_position[1] * (j + 1)
+        if j >= len(centered) - len(quote.get_authors()):
+            y -= 10
+
+        for i, char in enumerate(line):
+            font = fonts['icon'] if ord(char) > 512 else fonts['base']
+            char_width = draw.textlength(char, font=font)
+            if font == fonts['icon']:
+                draw.text((x, y + 10), char, fill=text_color, font=font, embedded_color=True)
+            else:
+                draw.text((x, y), char, fill=color, font=font, embedded_color=True)
+            x += char_width
+            if char == ':':
+                color = default_font_color
+
+    image.save("text_image.png")
+
 
 async def generate_image(engine: Engine):
     session = Session(engine)
     try:
-        width = 600
-        text_position = (50, 50)
-        text_color = (255, 255, 255)
-
-        fonts = {
-            "base": ImageFont.truetype("assets/Jaini-Regular.ttf", 36),
-            "icon": ImageFont.truetype("assets/SEGUIEMJ.ttf", 36),
-        }
-
         quote = get_random_quote(session)
-
-        if len(quote.get_authors()) == 1:
-            centered = prepare_quote(quote, width, text_position, fonts)
-        else:
-            centered = prepare_dialogue(quote, width, text_position, fonts)
-
-        image = Image.new("RGB", (width, 100 + 50 * len(centered)), (0x27, 0x29, 0x2E))
-        draw = ImageDraw.Draw(image)
-
-        for j, pair in enumerate(centered):
-            line, color = pair
-            x = text_position[0]
-            y = text_position[1] * (j + 1)
-            if j >= len(centered) - len(quote.get_authors()):
-                y -= 10
-
-            for i, char in enumerate(line):
-                font = fonts['icon'] if ord(char) > 512 else fonts['base']
-                char_width = draw.textlength(char, font=font)
-                if font == fonts['icon']:
-                    draw.text((x, y + 10), char, fill=text_color, font=font, embedded_color=True)
-                else:
-                    draw.text((x, y), char, fill=color, font=font, embedded_color=True)
-                x += char_width
-                if char == ':':
-                    color = default_font_color
-
-        image.save("text_image.png")
+        generate_image_for_quote(quote)
 
         quote.used = True
         session.commit()
