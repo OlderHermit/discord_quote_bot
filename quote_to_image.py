9aee88e018014d9d3551854be49ff12592b5ce4a
Zdzislaw <80225618+OlderHermit@users.noreply.github.com>
2026-05-29 00:58:39 +0200

    cleaned up the image generation code

--- quote_to_image.py
+++ quote_to_image.py
@@ -9,70 +9,63 @@ from sqlalchemy.orm import Session
 from orm import Quote, Author, Sentence
 
 default_font_color = (255, 255, 255)
+default_background_color = (0x27, 0x29, 0x2E)
 
-
-def check_size(text: str, check_font: ImageFont):
-    if check_font is not None:
-        return check_font.getlength(text)
+def get_size(text: str, font: FreeTypeFont):
+    if font:
+        return font.getlength(text)
     return 0
-
-def calculate_basic_widths(dialogue: Quote, author_offset, check_font):
-    space_width = check_size(' ', check_font)
-    author_widths = {
-        author: check_size(author.id + ':', check_font)
-        for author in dialogue.get_authors()
-    }
-    max_author_width = max(author_widths.values())
-    spaces_equal_max_author_width = math.ceil(max_author_width / space_width) + author_offset
-    return space_width, max_author_width, spaces_equal_max_author_width
 
 
 def format_author_line(author, space_width, max_author_width, author_offset, check_font):
-    author_width = check_size(author.id + ':', check_font)
-    spaces_needed = math.ceil((max_author_width - author_width) / space_width) + author_offset
+    author_width = get_size(author.id + ':', check_font)
+    spaces_needed = math.floor((max_author_width - author_width) / space_width) + author_offset
     return author.id + ':' + ' ' * spaces_needed
 
 
-def split_to_size(dialogue: Quote, maxsize: int, check_font: ImageFont):
+def split_to_size(dialogue: Quote, maxsize: int, font: FreeTypeFont):
     author_offset = 5
-    space_width, max_author_width, spaces_equal_max_author_width = calculate_basic_widths(dialogue, author_offset, check_font)
-
-    longest_size = 0
     result = []
+
+    space_width = get_size(' ', font)
+    max_author_width = max(get_size(a.id + ':', font) for a in dialogue.get_authors())
+    spaces_equal_max_author_width = math.ceil(max_author_width / space_width) + author_offset
 
     for sentence in dialogue.sentences:
         author = sentence.author
         text = sentence.sentence
         color = author.get_tuple_color()
 
-        line = format_author_line(author, space_width, max_author_width, author_offset, check_font)
-        empty_line = spaces_equal_max_author_width * ' '
-        combined_size = check_size(line, check_font)
+        for line in wrap_words(
+            text,
+            format_author_line(author, space_width, max_author_width, author_offset, font),
+            spaces_equal_max_author_width * ' ',
+            maxsize,
+            font
+        ):
+            result.append((line, color))
 
-        for i, word in enumerate(text.split(' ')):
-            if combined_size + check_size(word, check_font) > maxsize:
-                if longest_size < combined_size:
-                    longest_size = combined_size
-                result.append((line, color))
-                color = default_font_color
-                combined_size = max_author_width + check_size(word, check_font) + space_width
-                line = empty_line + word + ' '
-            else:
-                combined_size += check_size(word, check_font) + space_width
-                line += word + ' '
-
-        if longest_size < combined_size:
-            longest_size = combined_size
-        result.append((line, color))
-
-    result.append((center(generate_separator(longest_size, check_font), maxsize, check_font), default_font_color))
+    longest_size = max((get_size(line, font) for line, _ in result), default=0)
+    result.append((center(generate_separator(longest_size, font), maxsize, font), default_font_color))
     return result
 
+def wrap_words(words, first_prefix, cont_prefix, maxsize, font):
+    line = first_prefix
+    width = get_size(line, font)
+    for word in words.split(' '):
+        chunk = get_size(word + ' ', font)
+        if width + chunk > maxsize and line not in (first_prefix, cont_prefix):
+            yield line
+            line, width = cont_prefix, get_size(cont_prefix, font)
+        line += word + ' '
+        width += chunk
+    yield line
 
-def center(text: str, maxsize: int, check_font: ImageFont, emoji_font: ImageFont = None):
+
+def center(text: str, maxsize: int, font: FreeTypeFont, emoji_font: FreeTypeFont = None):
     split = split_emoji(text)
-    offset = maxsize - (check_size(split[0], check_font) + check_size(split[1], emoji_font))
-    space_width = check_size(' ', check_font)
+    offset = maxsize - (get_size(split[0], font) + get_size(split[1], emoji_font))
+    space_width = get_size(' ', font)
     spaces = math.ceil(offset / space_width)
     return ' ' * math.ceil(spaces / 2) + text + ' ' * math.ceil(spaces / 2)
 
@@ -84,19 +77,20 @@ def split_emoji(text: str):
     return result
 
 
-def generate_separator(longest_size: int, check_font: ImageFont):
-    sep_width = check_size('-', check_font)
+def generate_separator(longest_size: int, check_font: FreeTypeFont):
+    sep_width = get_size('-', check_font)
     seps = math.ceil(longest_size / sep_width)+2
     return '-'*seps
 
 
-def prepare_dialogue(quote: Quote, width: int, text_position: tuple[int, int], fonts: dict[str, FreeTypeFont]):
-    centered = split_to_size(quote, width - text_position[0] * 2, fonts['base'])
+def prepare_dialogue(quote: Quote, max_width: int, fonts: dict[str, FreeTypeFont]):
+    centered = split_to_size(quote, max_width, fonts['base'])
 
-    for author in quote.get_authors():
-        centered.append(
-            (center(author.signature, width - text_position[0] * 2, fonts['base'], fonts['icon']), author.get_tuple_color())
-        )
+    centered.extend(
+        (center(a.signature, max_width, fonts['base'], fonts['icon']), a.get_tuple_color())
+        for a in quote.get_authors()
+    )
+
     return centered
 
 
@@ -133,9 +127,9 @@ def generate_image_for_quote(quote: Quote):
         "icon": ImageFont.truetype("assets/SEGUIEMJ.ttf", 36),
     }
 
-    centered = prepare_dialogue(quote, width, text_position, fonts)
+    centered = prepare_dialogue(quote, width - text_position[0] * 2, fonts)
 
-    image = Image.new("RGB", (width, 100 + 50 * len(centered)), (0x27, 0x29, 0x2E))
+    image = Image.new("RGB", (width, 100 + 50 * len(centered)), default_background_color)
     draw = ImageDraw.Draw(image)
 
     for j, pair in enumerate(centered):
@@ -147,14 +141,14 @@ def generate_image_for_quote(quote: Quote):
 
         for i, char in enumerate(line):
             font = fonts['icon'] if ord(char) > 512 else fonts['base']
-            char_width = draw.textlength(char, font=font)
+            if char == ':':
+                color = default_font_color
+
             if font == fonts['icon']:
                 draw.text((x, y + 10), char, fill=text_color, font=font, embedded_color=True)
             else:
                 draw.text((x, y), char, fill=color, font=font, embedded_color=True)
-            x += char_width
-            if char == ':':
-                color = default_font_color
+            x += draw.textlength(char, font=font)
 
     image.save("text_image.png")
 
